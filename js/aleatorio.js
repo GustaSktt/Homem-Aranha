de83399056d8b668da28232b82cef81c99096125
GustaSktt <110995299+GustaSktt@users.noreply.github.com>
2024-08-26 11:51:14 -0300

    Add files via upload

---
+++ js/aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Gustavo", "Millena", "Amora", "Valdinei", "Jorge", "Naldo", "Apollo"];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
---
+++ js/perguntas.js
@@ -0,0 +1,173 @@
+export const perguntas = [
+    {
+        enunciado: "Após ser anunciado o filme, a população estava ficando loucas, algumas estavam ficando muito ansiosas com o filme, outras estavam com o pé atrás se este filme realmente prometia o que foi anunciado. Como você se sentiu ao saber deste filme?",
+        alternativas: [
+            {
+                texto: "Fiquei com o pé atrás.",
+                afirmacao: [
+                    "No início ficou com o pé atrás ",
+                    "Achou assustador pensar que este filme poderia lançar e o iludir."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Fiquei ansioso(a)!",
+                afirmacao: [
+                    "Quis saber como o filme contaria a história.",
+                    "Pensou que este filme seria o melhor filme do homem aranha."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "O mundo ficou em estado de choque, quais teorias surgiram imediatamente após o anúncio do filme?",
+        alternativas: [
+            {
+                texto: "Começaram a teorizar sobre a aparição os antigo Homens-Aranha.",
+                afirmacao: [
+                    "Acreditou fielmente nisso.",
+                    "Percebeu pelo trailer que existia essa possibilidade.",
+                    "Usou hack para descobrir."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Surgiu a teoria de outros vilões dos antigos filmes.",
+                afirmacao: [
+                    "Acreditou que os vilões retornariam.",
+                    "Acreditou na teoria mais provavel.",
+                    "Ficou com medo de acreditar na outra teoria."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "O mundo estava abalado, quais foram as reações dos estúdios concorrentes?",
+        alternativas: [
+            {
+                texto: "Os estúdios concorrentes entraram em pânico.",
+                afirmacao: [
+                    "Começaram a planejar suas próprias explorações do multiverso.",
+                    "Percebeu que iam ficar para atrás.",
+                    "Utilizaram novas ideias em blockbuster."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Os estúdios não ligaram para isso.",
+                afirmacao: [
+                    "Sentiu que não iam perder dinheiro.",
+                    "Decidiram pensar em fazer outro tipo de filme, ao invés de fazer que nem a Marvel.",
+                    "Estavam seguros que não ia ter problema."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Como a indústria do cinema mudou globalmente após o anúncio deste filme?",
+        alternativas: [
+            {
+                texto: "O conceito de multiverso se tornou uma tendência.",
+                afirmacao: [
+                    "Vários estúdios lançando produções de realidades alternativas.",
+                    "Assim novas possibilidades foram abertas."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "A indústria não se alterou.",
+                afirmacao: [
+                    "Esta forma não seria muito eficaz a longo prazo.",
+                    "Continuar pela forma tradicional seria mais eficaz."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais foram os impactos sociais e culturais desse choque global?",
+        alternativas: [
+            {
+                texto: "A expectativa gerou um fenômeno cultural.",
+                afirmacao: [
+                    "Gerando mais filmes com a tendência de multiverso, assim virando um caos.",
+                    "Inspirando uma nova onde de fãs e comunidades dedicadas à teoria e à análise."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "As pessoas logo esqueceriam deste filme.",
+                afirmacao: [
+                    "Foi só um hype momentâneo.",
+                    "Houveram algumas discussões, mas não foi adiante."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "O impacto cultural do filme foi limitado apenas aos fãs de quadrinhos e do MCU?",
+        alternativas: [
+            {
+                texto: "Alcançou uma audiência muito mais ampla.",
+                afirmacao: [
+                    "Atraiu mais fãs para o MCU.",
+                    "A marvel furou a bolha dos nerds."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Sim, somente os fãs foram assistir.",
+                afirmacao: [
+                    "O filme não conseguiu furar a bolha.",
+                    "O filme se limitou a agradar apenas os fãs."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Após o anúncio o interesse pelo personagem aumentou?",
+        alternativas: [
+            {
+                texto: "Aumentou e muito.",
+                afirmacao: [
+                    "Com a ideia de multiverso, as possibilidades eram enormes, assim o interesse só aumentou.",
+                    "Novos e antigos fãs se uniram em uma nova onda de entusiasmo."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Diminuiu.",
+                afirmacao: [
+                    "Até teve um interesse no momento, mas não durou muito.",
+                    "Diminuiu bastante."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "A revelação de que traria de volta os vilões icônicos de outras franquias foi recebida com indiferença?",
+        alternativas: [
+            {
+                texto: "Não, pelo contrário, ela gerou uma grande euforia.",
+                afirmacao: [
+                    "Ficaram ansiosos para ver como esses vilões entrariam neste universo.",
+                    "Este vilões voltando, voltaria a época de ouro dos filmes de heróis."
+                ],
+            },
+            {
+                texto: "Sim, pois deveria haver novos vilões.",
+                afirmacao: [
+                    "Os vilões antigos estavam ultrapassados!",
+                    "Novos vilões seriam melhores.",
+                    "Deviam ter mostrado o Galactus."
+                ],
+            },
+        ]
+    }
+];
